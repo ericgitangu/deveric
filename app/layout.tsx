394d8105588cbbaaf70bdc957b29cf68d57bed4a
Eric <Eric.Gitangu@rgare.com>
2023-05-12 15:06:34 -0500

    Initial commit.

---
+++ app/layout.tsx
@@ -0,0 +1,75 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+	title: {
+		default: "deveric.io",
+		template: "%s | deveric.io",
+	},
+	description: "Software engineer at upstash.com and founder of planetfall.io",
+	openGraph: {
+		title: "deveric.io",
+		description:
+			"Software engineer at upstash.com and founder of planetfall.io",
+		url: "https://deveric.io",
+		siteName: "deveric.io",
+		images: [
+			{
+				url: "https://deveric.io/og.png",
+				width: 1920,
+				height: 1080,
+			},
+		],
+		locale: "en-US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	twitter: {
+		title: "deveric",
+		card: "summary_large_image",
+	},
+	icons: {
+		shortcut: "/favicon.png",
+	},
+};
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+	src: "../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<body
+				className={`bg-black ${
+					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+				}`}
+			>
+				<Analytics />
+				{children}
+			</body>
+		</html>
+	);
+}
