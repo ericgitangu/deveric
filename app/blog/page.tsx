b66db006d0c36607401ae70338b0c31589bc82d8
Eric <Eric.Gitangu@rgare.com>
2023-05-13 00:13:29 -0500

    Add cursor to blog pointer.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -16,7 +16,7 @@ export default function About() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-                    Blog <span onClick={redirect} aria-hidden="true">&rarr;</span>
+                    Blog <span onClick={redirect} style={{cursor:"pointer"}} aria-hidden="true">&rarr;</span>
 			</h1>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
