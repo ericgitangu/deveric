871a5f7b33b50c56e1ad34dbaddcc685a88fc64f
Eric <developer.ericgitangu@gmail.com>
2023-12-12 08:43:28 -0600

    Typo..

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -25,7 +25,7 @@ export default function Certification() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-slate-400 ">
-					View  my skills verified and endorsed <a href='https://www.linkedin.com/in/ericgitangu/details/skills/' className="text-blue-700"> more certifcations </a>.
+					View  my skills verified and endorsed <a href='https://www.linkedin.com/in/ericgitangu/details/skills/' className="text-blue-700"> skilsets</a>.
 				</h2>
 			</div>
 		</div>
